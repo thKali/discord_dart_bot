157764e0cc846dd59d35b8d165939bb60eb77731
thKali <humbertochiesikali@gmail.com>
2022-08-19 19:47:30 -0300

    fix: token

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,5 +1,5 @@
 import 'package:nyxx/nyxx.dart';
 
-final String token = 'NzA0NDc1NjQ3MzA4NzI2MzQy.GB3Xq_.TZVzZBKwZPfUPXEGl7eYeatnSWAvQC4aCgZfpI';
+final String token = 'you token here';
 final int intents = GatewayIntents.all;
 final String prefix = '!';
